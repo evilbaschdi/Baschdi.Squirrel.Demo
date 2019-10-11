0d464ef3064299efe57fca126ecfa812f5e46464
Sebastian Walter <sebwalter@gmx.net>
2019-10-11 08:31:25 +0200

    first commit

---
+++ Baschdi.Squirrel.Demo/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using EvilBaschdi.Core;
+using Squirrel;
+
+namespace Baschdi.Squirrel.Demo
+{
+    public class CheckForUpdates : ICheckForUpdates
+    {
+        private readonly IUpdateManager _updateManager;
+
+        public CheckForUpdates(IUpdateManager updateManager)
+        {
+            _updateManager = updateManager ?? throw new ArgumentNullException(nameof(updateManager));
+        }
+
+
+        public async Task<UpdateInfo> TaskValue()
+        {
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            ServicePointManager.Expect100Continue = true;
+            var checkForUpdate = await _updateManager.CheckForUpdate();
+            return checkForUpdate;
+        }
+    }
+
+    public interface IUpdate : ITaskRun
+    { }
+
+    public class Update : IUpdate
+    {
+        private readonly IUpdateManager _updateManager;
+        private readonly ICheckForUpdates _checkForUpdates;
+
+
+        public Update(IUpdateManager updateManager, ICheckForUpdates checkForUpdates)
+        {
+            _updateManager = updateManager ?? throw new ArgumentNullException(nameof(updateManager));
+            _checkForUpdates = checkForUpdates ?? throw new ArgumentNullException(nameof(checkForUpdates));
+        }
+  
+
+        public async Task TaskRun()
+        {
+            var update = _checkForUpdates.TaskValue().Result;
+            
+            if (update.ReleasesToApply.Any())
+            {
+                await _updateManager.UpdateApp();
+            }
+        }
+    }
+
+    public interface ICheckForUpdates : ITaskValue<UpdateInfo>
+    {
+    }
+
+   
+}
---
+++ Baschdi.Squirrel.Demo/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Windows;
+using Squirrel;
+
+namespace Baschdi.Squirrel.Demo
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private async void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            var isGitHubMaintained = true;
+
+            IUpdateManager updateManager = new UpdateManager(Path.Combine(Directory.GetCurrentDirectory(), "Squirrel"));
+            if (isGitHubMaintained)
+            {
+                updateManager = await UpdateManager.GitHubUpdateManager("https://github.com/evilbaschdi/Baschdi.Squirrel.Demo", prerelease: true);
+            }
+
+            ICheckForUpdates checkForUpdates = new CheckForUpdates(updateManager);
+            IUpdate update = new Update(updateManager, checkForUpdates);
+            await update.TaskRun();
+
+            updateManager.Dispose();
+        }
+    }
+}
